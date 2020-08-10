7ac3af5f881347613ea993884e9fe66fcba106c2
jdbuysse <jdbuysse@gmail.com>
2020-08-10 17:29:53 -0600

    messing with highligher options

--- src/components/Snippet.js
+++ src/components/Snippet.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 //import {BrowserRouter as Router, Route} from 'react-router-dom';
-
+import Highlightable from "highlightable";
 
 const Snippet = (lessons) => {
   const API_URL = 'http://localhost:5000'
@@ -18,14 +18,37 @@ const Snippet = (lessons) => {
     })
   }, [lessons])
 
+  const onMouseOverHighlightedWordCallback = useCallback(() =>{
+    console.log('hi-lite')
+  }, []);
 
   return (
     <div>
       <Container>
         <Row>
-          <Col sm={{ size: 6, order: 2, offset: 0 }}>{lessons && lessons.lessons[0].body}</Col>
+          <Col sm={{ size: 6, order: 2, offset: 0 }}>
+          <Highlightable
+            ranges={[
+              {
+                start: 0,
+                end: 10,
+                text: "",
+                data: {}
+              }
+            ]}
+            enabled={true}
+            id={0}
+            onMouseOverHighlightedWord={onMouseOverHighlightedWordCallback()}
+            highlightStyle={{
+              backgroundColor: '#ffcc80'
+            }}
+            text={lessons && lessons.lessons[0].body}
+          />
+          </Col>
           <Col sm={{ size: 6, order: 2, offset: 0 }}><li>{annotations && annotations[0].content}</li><li>{annotations && annotations[1].content}</li></Col>
         </Row>
+        <Col>  
+        </Col>
       </Container>
     </div>
   );
