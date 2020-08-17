5842e07ba683404a51c643a999bbf52cae56a3c0
jdbuysse <jdbuysse@gmail.com>
2020-08-17 10:50:46 -0600

    placeholder cards for 'recent lessons'

---
+++ src/components/LessonCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Row, Col, CardGroup
+} from 'reactstrap';
+
+
+const LessonCard = () => {
+
+    return (
+        <div>
+            <Row>
+            <CardGroup>
+            <Card body>
+                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+            </CardGroup>
+            </Row>
+        </div>
+    )
+}
+
+export default LessonCard;
--- src/components/TeacherDashboard.js
+++ src/components/TeacherDashboard.js
@@ -1,46 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/teacherdashboard.css';
-import { Accordion, Card, Button } from 'react-bootstrap';
-import { Container, Row, Col} from 'reactstrap';
+import { Accordion, Card } from 'react-bootstrap';
+import { Row, Col } from 'reactstrap';
+import LessonCard from './LessonCard';
+
 
 const TeacherDashboard = () => {
+    const API_URL = 'http://localhost:5000'
+
+    const [lessons, setLessons] = useState()
+
+    useEffect(() => {
+        fetch(`${API_URL}/snippets/`)
+            .then(response => response.json())
+            .then(data => {
+                setLessons(data)
+            })
+    }, [])
 
     return (
         <div className="dashboard-wrapper">
             <Row>
-            <Col className="dashboard-accordions" sm={{ size: 8, order: 2, offset: 0 }}>
-            <Accordion defaultActiveKey="0">
-                <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="0" className="accordion-1">
-                            Recent Lessons
+                <Col className="dashboard-accordions" sm={{ size: 8, order: 2, offset: 0 }}>
+                    <Accordion defaultActiveKey="0">
+                        <Card>
+                            <Accordion.Toggle as={Card.Header} eventKey="0" className="accordion-1">
+                                Recent Lessons
                         </Accordion.Toggle>
-                    
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body>Hello! I'm the body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    
-                    <Accordion.Toggle as={Card.Header} eventKey="1">
-                            Classes
+
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>
+                                    <Row>
+                                    <div className="lesson-container">
+                                        {lessons && lessons.map((lesson, index) => (
+                                            <Col sm="6">
+                                                <LessonCard lesson={lesson} key={index} />
+                                            </Col>
+                                        ))}
+                                    </div>
+                                    </Row>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+
+                            <Accordion.Toggle as={Card.Header} eventKey="1">
+                                Classes
                     </Accordion.Toggle>
-                  
-                    <Accordion.Collapse eventKey="1">
-                        <Card.Body>Hello! I'm another body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    
-                    <Accordion.Toggle as={Card.Header} eventKey="1">
-                            Students
+
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body>Hello! I'm another body</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+
+                            <Accordion.Toggle as={Card.Header} eventKey="1">
+                                Students
                     </Accordion.Toggle>
-                  
-                    <Accordion.Collapse eventKey="1">
-                        <Card.Body>Hello! I'm another body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
-            </Col>
+
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body>Hello! I'm another body</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
+                </Col>
             </Row>
         </div>
     );
